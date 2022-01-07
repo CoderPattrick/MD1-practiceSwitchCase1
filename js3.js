722f7883b31ea5027e586bbb53e1ab4ac2ea340c
Dat <trandat251111@gmail.com>
2022-01-07 14:17:21 +0700

    add3

---
+++ js3.js
@@ -0,0 +1,9 @@
+let value = prompt('Type a number', 0);
+if (value > 0) {
+    alert(1);
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+//Switch case không dùng được ở if đkiện k bằng
